fb5146d675aee66611ee50c991e0f35c243b7b98
aishad <aishadiallo@gmail.com>
2018-05-02 17:41:55 -0400

    'final solution'

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,32 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.raw({ type: '*/*' }))
 
 let serverState = {
-    items: []
+    items: {}
 }
 
-app.get('/items', (req, res) => {
-    res.send(JSON.stringify(serverState.items));
+app.post('/items', (req, res) => {
+    let parsedBody = JSON.parse(req.body.toString())
+    if (!serverState.items[parsedBody.listName]) serverState.items[parsedBody.listName] = []
+    res.send(JSON.stringify(serverState.items[parsedBody.listName]));
 })
 
 app.post('/addItem', (req, res) => {
     let parsedBody = JSON.parse(req.body.toString())
     // The following could be rewritten in a shorter way using push.
     // I'm trying to ween everyone off of push
-    serverState.items = serverState.items.concat(parsedBody)
-    res.send(JSON.stringify(serverState.items));
+    if (!serverState.items[parsedBody.listName]) serverState.items[parsedBody.listName] = []
+    serverState.items[parsedBody.listName] = serverState.items[parsedBody.listName].concat(parsedBody.input)
+    res.send(JSON.stringify(serverState.items[parsedBody.listName]));
+})
+
+app.post('/delete', (req, res)=>{
+    let parsedBody = JSON.parse(req.body.toString())
+    res.send(JSON.stringify(serverState.items[parsedBody.listName]=[]))
+})
+
+app.post('/reverse', (req, res)=>{
+    let parsedBody = JSON.parse(req.body.toString())
+        res.send(JSON.stringify(serverState.items[parsedBody.listName].reverse()))
 })
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'))
